86b02c62c6c6134b893b0ee50450b6b99b67139a
sosina-bug <neth23nean67wwe.com@gmail.com>
2024-08-06 15:25:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+function searchCity(city) {
+let apiKey = "b2a5adcct04b33178913oc335f405433";
+let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+console.log(apiUrl);
+}
+
+
+
+
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
 
