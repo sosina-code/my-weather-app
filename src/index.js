a885fc6e7768f3d086076c1f4da90846de6046f5
sosina-bug <neth23nean67wwe.com@gmail.com>
2024-08-06 15:56:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -28,3 +28,4 @@ function handleSearchSubmit(event){
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit" , handleSearchSubmit);
+searchCity("paris");
