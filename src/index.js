e285da1fde4d2e62417fe9b7f682b648f0098fa2
sosina-bug <neth23nean67wwe.com@gmail.com>
2024-08-06 17:18:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function refreshWeather(response) {
 
 
     cityElement.innerHTML = response.data.city;
-    timeElement.innerHTML = `${date.getDay()} ${date.getHours()}:${date.getMinute()}`;
+    timeElement.innerHTML = `${date.getDay()}, ${date.getHours()}`;
     descriptionElement.innerHTML=response.data.condition.description;
     humidityElement.innerHTML=`${response.data.temperature.humidity}%`;
     temperatureElement.innerHTML = Math.round(temperature);
