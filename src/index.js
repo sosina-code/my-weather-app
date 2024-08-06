6a85a7d2d6af9afee82e7ae2b1657030bd688695
sosina-bug <neth23nean67wwe.com@gmail.com>
2024-08-06 16:52:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function refreshWeather(response) {
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
-    let windElement = document.querySelector("#wind-speed");
+    let windSpeedElement = document.querySelector("#wind-speed");
 
 
     cityElement.innerHTML = response.data.city;
